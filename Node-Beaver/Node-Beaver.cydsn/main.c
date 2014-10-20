4a25a8121be459982c68214de0be5056c81e7d2a
jongtao <jon.g.tao@gmail.com>
2014-10-19 18:17:43 -0700

    added test code for CAN

--- Node-Beaver/Node-Beaver.cydsn/main.c
+++ Node-Beaver/Node-Beaver.cydsn/main.c
@@ -21,7 +21,19 @@ int main()
 	CyIntSetVector(CAN_1_ISR_NUMBER, ISR_CAN_1);
 	CAN_1_GlobalIntEnable();
 
-	for(;;);
+	CAN_1_TX_MSG message;
+	message.id = 0x001;
+	message.rtr = 0;
+	message.ide = 0;
+	message.dlc = 8;
+	message.irq = 0;
+	message.msg->byte[0] = 0x01;
+	
+	for(;;)
+	{
+		CAN_1_SendMsg(&message);
+		CyDelay(500);
+	}
 
 	return 0;
 } /*main()*/
