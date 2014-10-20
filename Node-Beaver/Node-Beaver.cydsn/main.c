0f66a157843e1c6a9ffe5f60bb9d384f72472116
jongtao <jon.g.tao@gmail.com>
2014-10-19 17:53:13 -0700

    added empty interupt

--- Node-Beaver/Node-Beaver.cydsn/main.c
+++ Node-Beaver/Node-Beaver.cydsn/main.c
@@ -1,11 +1,27 @@
 /* Node-Beaver
-This node logs data via CAN and presents the data to the outside world.
+	 This node logs data via CAN and presents the data to the outside world.
 */
+
 #include <project.h>
+
+
+volatile CAN_1_TX_MSG message;
+
+
+
+CY_ISR(ISR_CAN_1)
+{
+} /*CAN Interupt*/
+
+
 
 int main()
 {
-    for(;;);
+	CAN_1_Start();
+	CyIntSetVector(CAN_1_ISR_NUMBER, ISR_CAN_1);
+	CAN_1_GlobalIntEnable();
+
+	for(;;);
 
 	return 0;
-}
+} /*main()*/
