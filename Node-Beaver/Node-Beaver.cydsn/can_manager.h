586b46e0e335fdf37e926bb9ada3ed2eb0c04026
jongtao <jon.g.tao@gmail.com>
2014-12-06 19:44:36 -0800

    fixed bad merge conflicts

--- Node-Beaver/Node-Beaver.cydsn/can_manager.h
+++ Node-Beaver/Node-Beaver.cydsn/can_manager.h
@@ -21,14 +21,14 @@ void can_init();
 
 uint8_t can_test_send();
 
-void can_get(DataPacket* data_queue, uint8_t* data_head, uint8_t* data_tail);
+void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail);
 
-void wrap_data_queue(uint8_t* data_head, uint8_t* data_tail);
+void wrap_data_queue(uint16_t* data_head, uint16_t* data_tail);
 
-inline void process_default(DataPacket* data_queue, uint8_t* data_head,
-	uint8_t* data_tail);
+inline void process_default(DataPacket* data_queue, uint16_t* data_head,
+	uint16_t* data_tail);
 
-inline void process_throttle(DataPacket* data_queue, uint8_t* data_head,
-	uint8_t* data_tail);
+inline void process_throttle(DataPacket* data_queue, uint16_t* data_head,
+	uint16_t* data_tail);
 
 #endif
