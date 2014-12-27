f1c5c8be28f8f8ec5fdfc6da5a70948ab3c0aff3
khjtony <khjtony@gmail.com>
2014-12-27 00:33:47 -0800

    write debug lib

--- Node-Beaver/Node-Beaver.cydsn/CbeeLib/CBeeDebug.h
+++ Node-Beaver/Node-Beaver.cydsn/CbeeLib/CBeeDebug.h
@@ -14,14 +14,6 @@ typedef struct CBeeDebug{
 }myDebug;
 
 
-void Debug_init(myDebug* obj,int queueSize){
-	int i=0;
-	*obj.errCount=0;
-	*obj.errQueue=(char**)malloc(DEBUG_QUEUE_SIZE*sizeof(char*));
-	for (i=0;i<DEBUG_QUEUE_SIZE;i++){
-		*obj.errQueue[i]=(char*)malloc(DEBUG_QUEUE_SIZE*sizeof(char));
-	}
-}
-
+void Debug_init(myDebug* obj,int queueSize);
 
 #endif
