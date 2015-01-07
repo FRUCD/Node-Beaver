6ba8334bcfa2f0bd4569e23bf142a424c44eee05
Hengjiu Kang <khjtony@gmail.com>
2015-01-07 01:38:32 -0800

    modify radio manager

--- Node-Beaver/Node-Beaver.cydsn/radio_manager.c
+++ Node-Beaver/Node-Beaver.cydsn/radio_manager.c
@@ -1,7 +1,22 @@
 #include "radio_manager.h"
 
+void myUART_Start(uint8_t option){
+    switch (option){
+        case 0:
+             UART_1_Start();
+            break;
+        default:
+            return;
+    }
+    return;
+}
+
 
 void radio_put(const DataPacket* data_queue, uint16_t data_head,
 	uint16_t data_tail)
 {
+    myUART_Start(RADIO_UART);
+    
 } // radio_put()
+
+
--- Node-Beaver/Node-Beaver.cydsn/radio_manager.h
+++ Node-Beaver/Node-Beaver.cydsn/radio_manager.h
@@ -4,6 +4,10 @@
 #include <project.h>
 #include "data.h"
 
+#define RADIO_UART 1
+   
+void myUART_Start(uint8_t option);
+   
 void radio_put(const DataPacket* data_queue, uint16_t data_head,
 	uint16_t data_tail);
 
