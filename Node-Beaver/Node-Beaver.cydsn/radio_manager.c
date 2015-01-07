09225ed369c18bb51d4be3400ee97ede72234005
Hengjiu Kang <khjtony@gmail.com>
2015-01-07 15:23:05 -0800

    deleted an unused variable

--- Node-Beaver/Node-Beaver.cydsn/radio_manager.c
+++ Node-Beaver/Node-Beaver.cydsn/radio_manager.c
@@ -67,7 +67,6 @@ void radio_get_bytes(const DataPacket* in_pkt,uint8_t* byte_ptr){
 void radio_put(const DataPacket* data_queue, uint16_t data_head,
 	uint16_t data_tail)
 {
-    uint8_t i=0;
     uint8_t all_data[DATA_LEN+1];
     myUART_Start(RADIO_UART);       //start uart_1
     radio_get_bytes(data_queue,all_data);
