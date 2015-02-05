db27ef50dc59d10d7bd41612cd7dead02e80aa21
jongtao <jon.g.tao@gmail.com>
2015-02-04 22:04:53 -0800

    cleaned up CAN

--- Node-Beaver/Node-Beaver.cydsn/can_manager.h
+++ Node-Beaver/Node-Beaver.cydsn/can_manager.h
@@ -1,25 +1,14 @@
 #ifndef CAN_MANAGER_H
 #define CAN_MANAGER_H
 
-
 #include <project.h>
 #include <stdio.h>
 #include "data.h"
 
-
-
 void can_init();
 
-uint8_t can_test_send();
+//uint8_t can_test_send();
 
 void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail);
-/*
-void wrap_data_queue(uint16_t* data_head, uint16_t* data_tail);
 
-inline void process_default(DataPacket* data_queue, uint16_t* data_head,
-	uint16_t* data_tail);
-
-inline void process_throttle(DataPacket* data_queue, uint16_t* data_head,
-	uint16_t* data_tail);
-*/
 #endif
