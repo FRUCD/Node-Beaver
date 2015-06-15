f0ef35a2f4c55abb41467a80e6f9839a64191a09
J. Tao <jon.g.tao@gmail.com>
2015-06-15 16:48:52 -0700

    cleaned and documented can_manager

--- Node-Beaver/Node-Beaver.cydsn/can_manager.c
+++ Node-Beaver/Node-Beaver.cydsn/can_manager.c
@@ -7,39 +7,6 @@ volatile uint16_t can_head = 0, can_tail = 0;
 
 
 
-void can_init()
-{
-	CAN_1_GlobalIntEnable(); // CAN Initialization
-	CAN_1_Init();
-	CAN_1_Start();
-} // can_init()
-
-
-
-/*
-uint8_t can_test_send()
-{
-	const uint8_t test_message[] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
-	uint8_t i, state;
-	CAN_1_TX_MSG message;
-	CAN_1_DATA_BYTES_MSG payload;
-
-	message.id = 0x001; // edit for testing
-	message.rtr = 0;
-	message.ide = 0;
-	message.dlc = 0x08;
-	message.irq = 1;
-	message.msg = &payload;
-
-	for(i=0;i<8;i++)
-		payload.byte[i] = test_message[i];
-
-	state = CAN_1_SendMsg(&message);
-	return state;
-} // can_test_send()
-*/
-
-
 /* can_get()
 	Takes data_queue, data_head, and data_tail.
 	Returns nothing.
@@ -48,7 +15,11 @@ uint8_t can_test_send()
 	according to their type, and enqueues data to data_queue. Wraps data_queue if
 	it is full. Finally, clears can_queue. 
 
-	Note that global variables can_queue, can_head, and can_tail are used.
+	Note that global extern variables can_queue, can_head, and can_tail are used.
+	Also, copies from volatile can_queue to non-volatile data_queue are atomic.
+
+	can_queue is filled from a CAN interrupt in:
+		"Generated\ Source/PSoC5/CAN_1_TX_RX_func.c"
 */
 void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail)
 {
@@ -82,3 +53,49 @@ void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail)
 	can_head = can_tail = 0;
 	CyExitCriticalSection(atomic_state); // END ATOMIC
 } // can_receive()
+
+
+
+/* can_init()
+	Takes nothing.
+	Returns nothing.
+
+	Initializes and start PSoC Can component
+*/
+void can_init(void)
+{
+	CAN_1_GlobalIntEnable(); // CAN Initialization
+	CAN_1_Init();
+	CAN_1_Start();
+} // can_init()
+
+
+
+/* can_test_send()
+	Takes nothing.
+	Returns Cypress' status number from CAN component
+
+	Sends a test message to the CAN.
+*/
+/*
+uint8_t can_test_send()
+{
+	const uint8_t test_message[] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+	uint8_t i, state;
+	CAN_1_TX_MSG message;
+	CAN_1_DATA_BYTES_MSG payload;
+
+	message.id = 0x001; // edit for testing
+	message.rtr = 0;
+	message.ide = 0;
+	message.dlc = 0x08;
+	message.irq = 1;
+	message.msg = &payload;
+
+	for(i=0;i<8;i++)
+		payload.byte[i] = test_message[i];
+
+	state = CAN_1_SendMsg(&message);
+	return state;
+} // can_test_send()
+*/
--- Node-Beaver/Node-Beaver.cydsn/can_manager.h
+++ Node-Beaver/Node-Beaver.cydsn/can_manager.h
@@ -5,10 +5,14 @@
 #include <stdio.h>
 #include "data.h"
 
-void can_init();
+
+
+void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail);
+
+void can_init(void);
+
 
 //uint8_t can_test_send();
 
-void can_get(DataPacket* data_queue, uint16_t* data_head, uint16_t* data_tail);
 
 #endif
